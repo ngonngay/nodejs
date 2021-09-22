021407784e98ca5dac64f3c5e5301df63e894524
ngonngay <thangnguyenquyetvlth@gamil.com>
2021-09-22 11:29:46 +0700

    ss8

---
+++ blog/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+// express handlebars
+app.engine('hbs', handlebars({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/tin-tuc', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
